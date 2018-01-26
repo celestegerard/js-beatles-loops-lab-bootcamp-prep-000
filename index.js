26b3eee7fad02f095f56cff77b7aab042a456611
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-26 21:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,12 @@ function johnLennonFacts(facts) {
   return array;
 }
 
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0
+  do{
+    array.push("I love the Beatles!")
+  } while (i < 15)
+  }
+  return array;
+}
